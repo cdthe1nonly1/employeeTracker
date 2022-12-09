8915f493c2220ec63b017b57792dda33138fd5af
Charles Dykstra <charles.b.dykstra@gmail.com>
2022-12-09 09:48:37 -0700

    Updated tables for input

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,17 +1,19 @@
 INSERT INTO department (name)
-VALUES ("Intro to JavaScript"),
-       ("Data Science"),
-       ("Linear Algebra"),
+VALUES ("IT"),
+       ("Human Resources"),
+       ("Finance"),
+       ("Legal"),
 
 INSERT INTO roles (title, salary, department)
-VALUES ("Intro to JavaScript"),
-       ("Data Science"),
-       ("Linear Algebra"),
+VALUES ("Engineer", 120000, 1),
+       ("Admin", 85000, 2),
+       ("Accountant", 95000, 3),
+       ("Lawyer", 150000, 4),
 
        
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Intro to JavaScript"),
-       ("Data Science"),
-       ("Linear Algebra"),
+VALUES ("Mike", "Edwards" 1, ),
+       ("Sue", "Smith", 2, ),
+       ("Dave", "Watts", 3, ),
 
        
